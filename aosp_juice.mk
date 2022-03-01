de28edf33f22d7464fce3ab0ceb826f5c278b58a
vigneshd2362 <100097546+vigneshd2362@users.noreply.github.com>
2022-02-28 20:12:37 -0800

    Update aosp_juice.mk

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -9,9 +9,6 @@ $(call inherit-product, device/xiaomi/juice/device.mk)
 # Inherit some common Project Arcana stuff.
 $(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_INCLUDE_PIXEL_CHARGER  := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_BLUR := true
 TARGET_GAPPS_ARCH := arm64
 
 # Bootanimation
@@ -20,7 +17,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Project Arcana Official
 ARCANA_OFFICIAL := true
 ARCANA_DEVICE := juice
-ARCANA_MAINTAINER := Don.Joaquin
+ARCANA_MAINTAINER := Vicky
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_juice
@@ -31,4 +28,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
